d14c780616a934deee3e1418c3893ec74a8df2a2
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2020-07-19 05:31:41 -0400

    azdo_repository: this is resolved successfully, but there is a bug that prevents it being used in azuredevops_build_definition

--- multi-stage/terraform/main.tf
+++ multi-stage/terraform/main.tf
@@ -2,27 +2,44 @@ variable org_service_url {}
 
 variable personal_access_token {}
 
-provider "azuredevops" {
+variable github_personal_access_token {}
+
+provider azuredevops {
   version = ">= 0.0.1"
   org_service_url = var.org_service_url
   personal_access_token = var.personal_access_token
 }
 
-data "azuredevops_project" "project" {
+data azuredevops_project project {
   project_name = "Cloud-Native"
 }
 
-data "azuredevops_git_repositories" "repository" {
+// this is resolved successfully, but there is a bug that prevents it being used in azuredevops_build_definition below
+data azuredevops_git_repositories azdo_repository {
   project_id = data.azuredevops_project.project.id
   name = "azdo-pipeline-design"
 }
 
-resource "azuredevops_build_definition" "pipeline" {
+output azdo_repository_id {
+  value = data.azuredevops_git_repositories.azdo_repository.id
+}
+
+resource azuredevops_serviceendpoint_github github {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = "Sample GithHub Personal Access Token"
+
+  auth_personal {
+    personal_access_token = var.github_personal_access_token
+  }
+}
+
+resource azuredevops_build_definition pipeline {
   project_id = data.azuredevops_project.project.id
   name = "azdo-pipeline-design-multi-stage"
   repository {
-    repo_id = data.azuredevops_git_repositories.repository.id
-    repo_type = "TfsGit"
-    yml_path = "multi-stage/pipeline.yaml"
+    repo_id = "fernandoespinosa/azdo-pipeline-design"
+    repo_type = "GitHub"
+    yml_path = "/multi-stage/pipeline.yaml"
+    service_connection_id = azuredevops_serviceendpoint_github.github.id
   }
 }
